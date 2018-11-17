cc890b8472deb208b0acf1e429294dd956f4ce2a
nuczzz <nuc_zzz@163.com>
2018-11-17 23:47:11 +0800

    prototype pattern

--- prototype/prototype.go
+++ prototype/prototype.go
@@ -1,6 +1,45 @@
 package prototype
 
+import "fmt"
+
 // Prototype Pattern Definition:
 // Specify the kinds of objects to create using a prototypical instance,
 // and create new objects by copying this prototype.
 
+type Prototype interface {
+	SetName(string)
+	SetHeight(int)
+	SetWeight(int)
+	Print()
+	Clone() Prototype
+}
+
+type Boy struct {
+	Name string
+	Height int
+	Weight int
+}
+
+func (b *Boy)SetName(name string) {
+	b.Name = name
+}
+
+func (b *Boy)SetHeight(height int) {
+	b.Height = height
+}
+
+func (b *Boy)SetWeight(weight int) {
+	b.Weight = weight
+}
+
+func (b *Boy)Print(){
+	fmt.Printf("name: %v\n", b.Name)
+	fmt.Printf("height: %v\n", b.Height)
+	fmt.Printf("weight: %v\n", b.Weight)
+	fmt.Println()
+}
+
+func (b *Boy)Clone() Prototype {
+	p := *b
+	return &p
+}
--- prototype/prototype_test.go
+++ prototype/prototype_test.go
@@ -1,1 +1,22 @@
 package prototype
+
+import "testing"
+
+func TestPrototype(t *testing.T) {
+	boy1 := &Boy{
+		Name: "boy1",
+		Height: 160,
+		Weight:58,
+	}
+	boy1.Print()
+
+	boy2 := boy1.Clone()
+	boy2.SetName("boy2")
+	boy2.SetWeight(70)
+	boy2.Print()
+
+	boy3 := boy2.Clone()
+	boy3.SetName("boy3")
+	boy3.SetHeight(180)
+	boy3.Print()
+}
