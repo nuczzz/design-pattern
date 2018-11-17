b0e1f7968e7a7b3e2e5161eec025e7783ecfb5a4
nuczzz <nuc_zzz@163.com>
2018-11-17 23:04:53 +0800

    add prototype pattern file

---
+++ prototype/prototype.go
@@ -0,0 +1,6 @@
+package prototype
+
+// Prototype Pattern Definition:
+// Specify the kinds of objects to create using a prototypical instance,
+// and create new objects by copying this prototype.
+
