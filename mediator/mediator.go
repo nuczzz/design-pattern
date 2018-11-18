6350bcd84aa6bb61b91d0a6cf1f2f49984ca598f
nuczzz <nuc_zzz@163.com>
2018-11-18 22:39:20 +0800

    mediator pattern

--- mediator/mediator.go
+++ mediator/mediator.go
@@ -1,6 +1,35 @@
 package mediator
 
+import (
+	"fmt"
+)
+
 // Mediator Pattern Definition:
 // Define an object that encapsulates how a set of objects interact.
 // Mediator promotes loose coupling by keeping objects from referring to each other explicitly,
 // and it lets you vary their interaction independently.
+
+type User struct {
+	Name string
+}
+
+func NewUser(name string) User {
+	return User{
+		Name: name,
+	}
+}
+
+type Mediator interface {
+	SendMessage(from, to User, msg string)
+}
+
+type ChatRoom struct {
+}
+
+func (c *ChatRoom) SendMessage(from, to User, msg string) {
+	fmt.Printf("[%v]->[%v]: %v\n", from.Name, to.Name, msg)
+}
+
+func NewChatRoom() Mediator {
+	return &ChatRoom{}
+}
--- mediator/mediator_test.go
+++ mediator/mediator_test.go
@@ -1,1 +1,12 @@
 package mediator
+
+import "testing"
+
+func TestChatRoomSendMessage(t *testing.T) {
+	room := NewChatRoom()
+	boy := NewUser("boy")
+	girl := NewUser("girl")
+	room.SendMessage(boy, girl, "I love you!")
+	room.SendMessage(girl, boy, "But I don't love you...")
+	room.SendMessage(boy, girl, "...")
+}
