9ae0307cb4d8a6f6c91824529f16fcba0592fc18
nuczzz <nuc_zzz@163.com>
2018-11-19 23:43:15 +0800

    add annotaion

--- command/command.go
+++ command/command.go
@@ -1,1 +1,5 @@
 package command
+
+// Command Pattern Definition:
+// Encapsulate a request as an object,thereby letting you parameterize clients with different requests,
+// queue or log requests,and support undoable operations.
