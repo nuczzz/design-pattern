b1e3ecb13442816dc7c2f23b0003ad5afd676ccc
nuczzz <nuc_zzz@163.com>
2018-11-27 23:20:03 +0800

    add adapter pattern file

---
+++ adapter/adapter.go
@@ -0,0 +1,5 @@
+package adapter
+
+// Adapter Pattern Definition:
+// Convert the interface of a class into another interface clients expect.
+// Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
