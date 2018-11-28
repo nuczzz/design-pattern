36b32cff23d3003ae9a45015fad330e8ff66b461
nuczzz <nuc_zzz@163.com>
2018-11-28 23:40:25 +0800

    adapter pattern

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -1,5 +1,48 @@
 package adapter
 
+import "fmt"
+
 // Adapter Pattern Definition:
 // Convert the interface of a class into another interface clients expect.
 // Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
+
+type Player interface {
+	Play()
+}
+
+type VideoPlayer struct {
+}
+
+func NewVideoPlayer() Player {
+	return &VideoPlayer{}
+}
+
+func (vp *VideoPlayer) Play() {
+	fmt.Println("video play...")
+}
+
+type Movie struct {
+}
+
+func (m *Movie)  PlaySound() {
+	fmt.Println("movie play sound...")
+}
+
+func (m *Movie) PlayPicture() {
+	fmt.Println("movie play picture...")
+}
+
+type MovieAdapter struct {
+	movie *Movie
+}
+
+func (ma *MovieAdapter) Play() {
+	ma.movie.PlaySound()
+	ma.movie.PlayPicture()
+}
+
+func NewMoviePlayer() Player {
+	return &MovieAdapter{
+		movie: &Movie{},
+	}
+}
--- adapter/adapter_test.go
+++ adapter/adapter_test.go
@@ -1,1 +1,15 @@
 package adapter
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestAdapter(t *testing.T) {
+	videoPlay := NewVideoPlayer()
+	moviePlayer := NewMoviePlayer()
+
+	videoPlay.Play()
+	fmt.Println()
+	moviePlayer.Play()
+}
