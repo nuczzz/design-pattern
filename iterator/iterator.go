178db8dcaec6ba12072c0054cae701c61b877ee3
nuczzz <nuc_zzz@163.com>
2018-11-29 23:34:14 +0800

    iterator pattern

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -3,3 +3,33 @@ package iterator
 // Iterator Pattern Definition:
 // Provide a way to access the elements of an aggregate object sequentially
 // without exposing its underlying representation.
+
+type Iterator interface {
+	HasNext() bool
+	Next()
+	Value() interface{}
+}
+
+type Array struct {
+	list  []int
+	index int //index of list
+}
+
+func (a *Array) HasNext() bool {
+	return a.index < len(a.list)
+}
+
+func (a *Array) Next() {
+	a.index++
+}
+
+func (a *Array) Value() interface{} {
+	return a.list[a.index]
+}
+
+func NewListIterator(list []int) Iterator {
+	return &Array{
+		list: list,
+		index:  0,
+	}
+}
--- iterator/iterator_test.go
+++ iterator/iterator_test.go
@@ -1,1 +1,15 @@
 package iterator
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestIterator(t *testing.T) {
+	list := []int{1, 2, 3, 4}
+	iterator := NewListIterator(list)
+	for iterator.HasNext() {
+		fmt.Println(iterator.Value())
+		iterator.Next()
+	}
+}
