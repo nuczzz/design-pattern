b50fc2c14c59e49e10d2edc685fc7dc956272da8
nuczzz <nuc_zzz@163.com>
2018-11-29 23:39:10 +0800

    add composition pattern annotaion

--- composite/composite.go
+++ composite/composite.go
@@ -1,1 +1,5 @@
 package composite
+
+// Composite Pattern Definition:
+// Compose objects into tree structures to represent part-whole hierarchies.
+// Composite lets clients treat individual objects and compositions of objects uniformly.
