04975579822f9cab3d68573f600ef2c72150190f
nuczzz <nuc_zzz@163.com>
2018-12-01 23:11:36 +0800

    composite pattern

--- composite/composite.go
+++ composite/composite.go
@@ -3,3 +3,14 @@ package composite
 // Composite Pattern Definition:
 // Compose objects into tree structures to represent part-whole hierarchies.
 // Composite lets clients treat individual objects and compositions of objects uniformly.
+
+type Composite interface {
+	Name() string
+	Description() string
+	Price() float64
+	Print()
+	Add(Composite)
+	Remove(int)
+	Child(int) Composite
+}
+
