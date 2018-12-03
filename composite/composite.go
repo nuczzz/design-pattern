6281a227f0903cd477150670b2d9ded1dadcdbd8
nuczzz <nuc_zzz@163.com>
2018-12-03 23:34:40 +0800

    composite pattern

--- composite/composite.go
+++ composite/composite.go
@@ -1,16 +1,50 @@
 package composite
 
+import "fmt"
+
 // Composite Pattern Definition:
 // Compose objects into tree structures to represent part-whole hierarchies.
 // Composite lets clients treat individual objects and compositions of objects uniformly.
 
 type Composite interface {
-	Name() string
-	Description() string
-	Price() float64
-	Print()
-	Add(Composite)
-	Remove(int)
-	Child(int) Composite
+	Add(composite Composite)
+	Remove(composite Composite)
+	GetEList() []Composite
+	Print() string
 }
 
+type Employee struct {
+	name   string
+	dept   string
+	salary float64
+	eList  []Composite
+}
+
+func (e *Employee) Add(composite Composite) {
+	e.eList = append(e.eList, composite)
+}
+
+func (e *Employee) Remove(composite Composite) {
+	for i := range e.eList {
+		if e.eList[i] == composite {
+			e.eList = append(e.eList[:i], e.eList[i+1:]...)
+			return
+		}
+	}
+}
+
+func (e *Employee) GetEList() []Composite {
+	return e.eList
+}
+
+func (e *Employee) Print() string {
+	return fmt.Sprintf("Employee: %v, dept: %v, salary: %v\n", e.name, e.dept, e.salary)
+}
+
+func NewEmployee(name, dept string, salary float64) Composite {
+	return &Employee{
+		name:   name,
+		dept:   dept,
+		salary: salary,
+	}
+}
--- composite/composite_test.go
+++ composite/composite_test.go
@@ -1,1 +1,31 @@
 package composite
+
+import (
+	"testing"
+	"fmt"
+)
+
+func printAll(composite Composite) {
+	for _, e := range composite.GetEList() {
+		fmt.Print(e.Print())
+		printAll(e)
+	}
+}
+
+func TestComposite(t *testing.T) {
+	ceo := NewEmployee("zhao", "ceo", 1000)
+	cto := NewEmployee("qian", "cto", 1000)
+	dev1 := NewEmployee("dev1", "dev", 100)
+	dev2 := NewEmployee("dev2", "dev", 100)
+	stu1 := NewEmployee("stu1", "stu", 50)
+	stu2 := NewEmployee("stu2", "stu", 50)
+
+	ceo.Add(cto)
+	cto.Add(dev1)
+	cto.Add(dev2)
+	dev1.Add(stu1)
+	dev2.Add(stu2)
+
+	fmt.Print(ceo.Print())
+	printAll(ceo)
+}
