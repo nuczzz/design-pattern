da8d6862fa9ef8fbf92ef3145f888993602f5ba8
nuczzz <nuc_zzz@163.com>
2018-12-05 23:40:21 +0800

    facade pattern

--- facade/facade.go
+++ facade/facade.go
@@ -1,5 +1,44 @@
 package facade
 
+import "fmt"
+
 // Facade Pattern Definition:
 // Provide a unified interface to a set of interfaces in a subsystem.
 // Facade defines a higher-level interface that makes the subsystem easier to use.
+
+type Facade struct {
+	M Music
+	V Video
+	C Comment
+}
+
+func (f *Facade) Play() {
+	f.M.GetMusic()
+	f.V.GetVideo()
+	f.C.GetComments()
+}
+
+type Music struct {
+}
+
+func (m *Music) GetMusic() {
+	fmt.Println("play music...")
+}
+
+type Video struct {
+}
+
+func (v *Video) GetVideo() {
+	fmt.Println("play video...")
+}
+
+type Comment struct {
+}
+
+func (c *Comment) GetComments() {
+	fmt.Println("show comments...")
+}
+
+func NewMovie() *Facade {
+	return &Facade{}
+}
--- facade/facade_test.go
+++ facade/facade_test.go
@@ -1,1 +1,8 @@
 package facade
+
+import "testing"
+
+func TestFacade(t *testing.T) {
+	movie := NewMovie()
+	movie.Play()
+}
