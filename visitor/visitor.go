3f2257f7ab42a3255305045b3bb3cefb38b70ca9
nuczzz <nuc_zzz@163.com>
2018-12-06 23:13:27 +0800

    add visitor pattern file

---
+++ visitor/visitor.go
@@ -0,0 +1,5 @@
+package visitor
+
+// Visitor Pattern Definition:
+// Represent an operation to be performed on the elements of an object structure.
+// Visitor lets you define a new operation without changing the classes of the elements on which it operates.
