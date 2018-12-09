2ec7f890e804a44b217d7e49f71b329ac3e1e375
nuczzz <nuc_zzz@163.com>
2018-12-10 00:05:07 +0800

    update interpreter pattern

--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -8,9 +8,9 @@ import (
 func TestInterpreter(t *testing.T) {
 	john := NewBaseExpression("john")
 	tall := NewBaseExpression("tall")
-	robert := NewBaseExpression("robert")
-	cute := NewBaseExpression("cute")
+	nobody := NewBaseExpression("nobody")
+	nameless := NewBaseExpression("nameless")
 
 	fmt.Println("john is tall? ", NewAndExpression(john, tall).Interpret("john is tall?"))
-	fmt.Println("robert is ugly? ", NewOrExpression(robert, cute).Interpret("ugly"))
+	fmt.Println("john or his brother is here? ", NewOrExpression(nobody, nameless).Interpret("john or his brother"))
 }
