cc28cccb1c34e38e6c3dcc804237d27066eca838
nuczzz <nuc_zzz@163.com>
2018-12-11 23:54:25 +0800

    flyweight pattern

--- flyweight/flyweight.go
+++ flyweight/flyweight.go
@@ -1,6 +1,35 @@
 package flyweight
 
+import (
+	"fmt"
+)
+
 // Flyweight Pattern Definition:
 // Use sharing to support large numbers of fine-grained objects efficiently.
 
-// todo
+type Element struct {
+	key   string
+	color string
+}
+
+var m = make(map[string]*Element)
+
+func (e *Element)GetColor() string {
+	return e.color
+}
+
+func NewElement(key string, color string) *Element {
+	if v, ok := m[key]; ok {
+		fmt.Printf("element [key: %v] has exist\n", key)
+		v.color = color
+		return v
+	}
+
+	fmt.Printf("create new element [key: %v, color: %v]\n", key, color)
+	e := &Element{
+		key:   key,
+		color: color,
+	}
+	m[key] = e
+	return e
+}
--- flyweight/flyweight_test.go
+++ flyweight/flyweight_test.go
@@ -1,1 +1,9 @@
 package flyweight
+
+import "testing"
+
+func TestFlyweight(t *testing.T) {
+	NewElement("john", "red")
+	NewElement("julia", "green")
+	NewElement("john", "blue")
+}
